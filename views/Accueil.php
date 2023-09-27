bc0cc05bb95a60f5007d2da2bcb81773a8fb9ae6
Dindon03 <Dondelinger-jonathan@hotmail.fr>
2023-09-27 17:37:05 +0200

    au secours

---
+++ views/Accueil.php
@@ -0,0 +1,23 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>TEST</h1>
+    <?php foreach ($professional as $professional) {
+    ?>
+
+    <div class="pro">
+        <p><?= htmlspecialchars($professional['raisonSociale'])  ?></p>
+        <p><?= htmlspecialchars($professional['telephone'])  ?></p>
+    </div>
+    
+    <?php
+    }
+    ?>
+
+</body>
+</html> -->
