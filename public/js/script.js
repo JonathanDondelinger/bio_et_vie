0691ae128dafd17280466252a233534ab0edeb6c
Dindon03 <Dondelinger-jonathan@hotmail.fr>
2023-11-03 17:10:08 +0100

    responsive 480 menu burger

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,5 @@
 let burgerMenu = document.getElementById('burger-menu');
 let overlay = document.getElementById('menu');
 burgerMenu.addEventListener('click', function () {
-  this.classList.toggle("close");
-  overlay.classList.toggle("overlay");
+  overlay.classList.toggle("active");
 });
